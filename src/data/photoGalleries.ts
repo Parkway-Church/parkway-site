a23e6fd56a5e602e83c198acd6bdb5b92f56dcc4
Sam Castro <castro.samuel@gmail.com>
2026-06-15 17:53:12 -0400

    feat: add 17 Google Photo albums to the gallery page in chronological order

--- src/data/photoGalleries.ts
+++ src/data/photoGalleries.ts
@@ -8,8 +8,110 @@ export interface PhotoGallery {
 export const photoGalleries: PhotoGallery[] = [
     {
         id: 'gallery-1',
+        title: 'Illumination Station VBS 2026',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczM9QhpBu5oZQrSKIAqSREzYjgBdCkO7aNYBDmzrxOsX8Apei7FVjhVzYWr2BtcIEwe3AkPcEAcvLHMRo3qB-cMJfKi9k3HC70WGFWpDFLP4IfDHUKg=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/UPYmQc4SVxNu74kt9'
+    },
+    {
+        id: 'gallery-2',
+        title: 'The Park Youth 2026 Activities',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczO9Ccjy71ZQRf1LpdkOGG6CxJQ94h_vrioZdAj8YcM-yUWgaOjWKsT-mIU4hdjSBSQaTHmc1RGvGRBDn3g46PkWGaNwVd-sf2epaRM7toJ_eZqamUs=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/yiPU2Y4m274txQmA9'
+    },
+    {
+        id: 'gallery-3',
+        title: "Mother's Day 2026",
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczPmgjKEbQgnv-K74OA-q2oSrrXyJb_iGa5kmoOMzZvskhsqWXApgXEACRERb7UML9RW5UzSFuW_8PrwlKUAnAtlrFVbB8wc5ANR7E56pLn-LqoeTys=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/jK3LrYSXKwjcfCxH9'
+    },
+    {
+        id: 'gallery-4',
+        title: 'Life at Parkway 2025-2026',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczPahw8Sv-A5XXm2ZUpcfIIR0cKCPgJ3bYXc3-eOrwxr-LHvQI5uNgXuVehrh046hTylAvbKhhWnWVBZhsS3TbA2w64T4BTy2VxTzbpg7j0FWrLWoEQ=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/Fc2SA7S8WU5r2TWYA'
+    },
+    {
+        id: 'gallery-5',
+        title: 'Regional Assembly 2026',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczNd-ndUZ8y4aQ91FHjUY5DNV9_pst8UwwzDwBXbRRlF_3CW5OTH-POYMON85oZYxh_iIngPUx2u-VcFsaXbt6LIllqHVpRD5C7JK93-UuuoRKrpcmg=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/axsyHCd67ggf5jzt9'
+    },
+    {
+        id: 'gallery-6',
+        title: 'Easter Sunrise Service 2026',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczMNvfR_YNJwOgKa05rsu0987cQCL0ezcmyX86iCQFn0lv0EScNacxS0cbi4Nwz_Ggjq4CAq-W3XK-IiMXQZnZHTF8o2_x1xV0q8HPWIyL1B2pOnt5w=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/ct5dAbVGNzR1hH3r7'
+    },
+    {
+        id: 'gallery-7',
+        title: 'Easter Egg Hunt 2026',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczMFmJlUHLGrOAIXhsrpXV0LtjAEXiGk5r6U-r6AY6wFuSeEmfPZrGRxsGkDofXIYWxsizIY-zmqU0eKKcQ_SnF2_Z_4rp13wxC7fJ3lPSP2mr1xh1s=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/f2dDGsQm4tuZGpQU6'
+    },
+    {
+        id: 'gallery-8',
+        title: "Men's Gatherings 2026",
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczMwxjwceORB6qLqT5NJzBipaAVsrDlAeAO4IBXxrew1pltcPfcbFDYcC-jqEPJDI-NAgU94KX9UdP9z9QqefZiLHbx6fUMzGXiIVehap-gAj5KfGJU=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/kqz4Zt4pVzDCZDBUA'
+    },
+    {
+        id: 'gallery-9',
+        title: 'A Table for Two Date Night Event 2026',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczOCbTGwv1rYyZLlx9e-8A2YdxNdET_0zYf7t2Azcmpgq_I_HObZfFd5y0e9PZ8n72jEYn61nsZ_EM_YOlU8sLDE3vsgf-8hd-MfN6eyZhKhz3pudaA=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/QTKLY6QW6NRncw687'
+    },
+    {
+        id: 'gallery-10',
+        title: 'Chapel Services with Pastor Esther',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczOmu8QErUY9hhML4s7YeZ4Gx7X87za2EuC_DqYwdBNmpYf7p_pFfZSc6zRojqZQiF5PlfUcLi4bbfdAdYOGRCM7ur4vReQGISfAZ1UjJaTOIDyHzf0=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/eByHcYntfWmREH1R9'
+    },
+    {
+        id: 'gallery-11',
+        title: '2025 Mixed Activities',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczOxz8kK17GEKUjZg-0zYly8dYaYGIxaqjLg-Di-mDahr9PDbzmMsUJ2X0SzxPncXXZBdxbG9If9h-buxSyOU1ei_VJfX-IATLnvUVZur5nldyhEmao=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/pg4H3pzZE4MVqMys9'
+    },
+    {
+        id: 'gallery-12',
+        title: 'Bethlehem Experience 2025',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczNFU_suvnVj0X-8jF6qlcaU58OfSAkuuLz8Pfk_Jq68pNPuSp4qm3FrIRG5P62Yrrb2AHqJ19oBOYxvB1xaSVTrkkOWzuotwVVHFHBlhmRFy1eAuwE=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/GkrZ484u5vPFQrwA7'
+    },
+    {
+        id: 'gallery-13',
+        title: 'Hoedown 2025',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczMaT6g3KuFq4MMoZ49PUdXU7rhmeV-bKYsTTDAhVHm2t1a6dHOv--OLavb5Qrw7ToDNIJfg5wQ3yr4FUoLC0ORO7U21ou6HtSW-WeL49ugvyj1cmNc=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/MndzU1oAsqYAy9ib7'
+    },
+    {
+        id: 'gallery-14',
         title: 'VBS 2025',
         coverImage: '/images/vbs-main.jpg',
         googlePhotosUrl: 'https://photos.app.goo.gl/Xe9BjmSw6rCG1YNK9'
+    },
+    {
+        id: 'gallery-15',
+        title: 'Christmas Play 2024',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczOIWsAH2jDyuDPj8RJ1xD-NiM8WL0y8uMptR_B3KPSkBI2iTBy2KeIHh7fAaLb9sseoBrRqW61Ek8iZogT37Hgxzd4nI0VvvBQU9WDGhpzHmFAh-Io=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/HGNvQ67Ffk6dgEqd7'
+    },
+    {
+        id: 'gallery-16',
+        title: 'Trunk or Treat 2024',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczPMZ-IrEeiXx61ov08RIGNbQwRgvfx0npHgWS0aq3dgbVrY6Q2sLfRdUmvpu2lmsgPGRYKZB9HV0o3edBgVFy7mqnXM4PtKHZTV22mrQy-0dnfcakE=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/awpT5LTbtViBv9BW6'
+    },
+    {
+        id: 'gallery-17',
+        title: 'Mixed Activities 2024',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczNYVdDZlmr04LSOnN7yURq2XiIQizEE2HlQR6s4ePGemmfZPrkRO6FHM9Sjtj-uQzd4L_UT4kXKaOOK2HClqjx1HmNEQvCaNvrD4rP6ahfjWms-Zp0=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/ErjWmoaaWQdtckYy9'
+    },
+    {
+        id: 'gallery-18',
+        title: 'The Park Youth - Rock the Universe 2023 Trip',
+        coverImage: 'https://lh3.googleusercontent.com/pw/AP1GczMdYU3pwGtdjBdgM7zKwgp15w8az8CJ42221nhc7c10siXNjkIT7_Rj8lwA_vM8T2VKWOA_ilonPs1AFaudkvLb25s1jyqGJmZLqUDf4-nUPOiRNzY=w600-h315-p-k',
+        googlePhotosUrl: 'https://photos.app.goo.gl/8MjHdvW7k7hQa3QP7'
     }
 ];
